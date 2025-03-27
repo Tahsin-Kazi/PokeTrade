ca636935626a35a6cb43f5416b2939d624c688a9
Jason-Bogtong <jason.bogtong@gmail.com>
2025-03-27 19:17:34 -0400

    Continued working on marketplace

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def postPokemon(request, id):
+    Pokemon = get_object__or_404(request, dexNumber)
+    template_data = {
+        'name' = Pokemon.name,
+        price = input('Price: ')
+    }
+
+    if request.method == 'POST':
+        return
+
+
+def editPrice(request):
+
+return redirect('marketplace.show')
+
+
+
