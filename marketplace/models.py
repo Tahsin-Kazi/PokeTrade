0a4590e86245165809c19a3a0c7d41e830b865c1
Jason-Bogtong <jason.bogtong@gmail.com>
2025-04-01 18:18:42 -0400

    Changed marketplace

--- marketplace/models.py
+++ marketplace/models.py
@@ -4,9 +4,12 @@ from collection.models import Pokemon
 
 
 class marketPlacePokemon(models.Model):
-    pokemon = Pokemon.objects.get(id=1)
+    pokemon = models.ForeignObject(Pokemon)
     price = models.FloatField()
     seller = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
-        return str(self.pokemon)
+        return "Pokemon: " + self.pokemon.name + " Seller :" + self.seller.username + " Price: " + str(self.price)
 
+
+class auctionHouse(models.Model):
+    marketPlacePokemon = models.ForeignKey(marketPlacePokemon, on_delete=models.CASCADE)
