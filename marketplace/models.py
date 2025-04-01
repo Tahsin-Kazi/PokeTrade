dd57bdb6402b1b3de95f4aa02bd5e7201e49bee1
MaximusPrime-18 <mdw2023@gmail.com>
2025-04-01 18:15:06 -0400

    New changes

--- marketplace/models.py
+++ marketplace/models.py
@@ -3,10 +3,6 @@ from django.db import models
 from collection.models import Pokemon
 
 
-class marketPlacePokemon(models.Model):
-    pokemon = Pokemon.objects.get(id=1)
+class Listing(models.Model):
     price = models.FloatField()
     seller = models.ForeignKey(User, on_delete=models.CASCADE)
-    def __str__(self):
-        return str(self.pokemon)
-
