48cd3f49d86c1d5bd67a6b645883c55fdb7f94f0
Ahad Sattar <ahadsattar007@gmail.com>
2025-04-06 20:06:51 -0400

    Fixed immediate backend for friends

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,4 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name = "home.index"),
-    path("", views.index, name = "friends.index"),
-    path('', views.index, name='collection.index'),
-    # path("about/", views.about, name = "home.about")
 ]
--- poketrade/urls.py
+++ poketrade/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("home.urls")),
     path('collection/', include("collection.urls")),
+    path('friends/', include("friends.urls")),
     path("", include("accounts.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
