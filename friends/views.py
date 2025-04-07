9b196c78ca468b65ae2617403e5583adfb70c1a8
Ahad Sattar <ahadsattar007@gmail.com>
2025-04-06 20:28:54 -0400

    Made basic friends page to match current funtionality

--- friends/views.py
+++ friends/views.py
@@ -5,6 +5,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 @login_required(login_url='login')
 def index(request):
     user = request.user
-    friends = user.profile.friends.all()
+    profile = user.profile
+    friends = profile.friends.all()
+
+    query = request.GET.get('q', '')  
+    if query:
+        friends = profile.friends.filter(username__icontains=query)
+    else:
+        friends = profile.friends.all()  
 
     return render(request, 'friends/index.html', {'friends': friends})
