7e2714af9d16b5543f6ccf4fbaf2af3855c0aff3
MaximusPrime-18 <mdw2023@gmail.com>
2025-04-11 15:48:33 -0400

    Create a form for the Friends Message

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Message
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -13,3 +14,8 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+    class MessageForm(forms.Form):
+        class Meta:
+            model = Message
+            fields = ['recipient', 'subject', 'body']
