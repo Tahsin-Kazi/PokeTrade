3abee032b12437db8188e926e3304ebcfde41bff
MaximusPrime-18 <mdw2023@gmail.com>
2025-04-11 17:18:32 -0400

    Condensing Friends Model into Profile class

--- accounts/models.py
+++ accounts/models.py
@@ -10,6 +10,18 @@ class Profile(models.Model):
     friends = models.ManyToManyField(User, blank=True, related_name='friends')
     currency = models.PositiveBigIntegerField()
 
+    from_user = models.ForeignKey(User, related_name='sent_requests', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name='received_requests', on_delete=models.CASCADE)
+    is_accepted = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('from_user', 'to_user')  # Prevent duplicate requests
+
+    def __str__(self):
+        return f"{self.from_user} ➡️ {self.to_user}"
+
+
     def __str__(self):
         return f"{self.user.username}"
 
@@ -28,17 +40,6 @@ class Profile(models.Model):
             Profile.objects.create(user=instance)
 
 
-class FriendRequest(models.Model):
-    from_user = models.ForeignKey(User, related_name='sent_requests', on_delete=models.CASCADE)
-    to_user = models.ForeignKey(User, related_name='received_requests', on_delete=models.CASCADE)
-    is_accepted = models.BooleanField(default=False)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ('from_user', 'to_user')  # Prevent duplicate requests
-
-    def __str__(self):
-        return f"{self.from_user} ➡️ {self.to_user}"
 
 def get_friends(user):
     sent = FriendRequest.objects.filter(from_user=user, is_accepted=True).values_list('to_user', flat=True)
