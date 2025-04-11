cb8d9631b12f834df0be965d2a809cdfc7a250cc
Jason-Bogtong <jason.bogtong@gmail.com>
2025-04-11 18:15:18 -0400

    Changes to Marketplace

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from datetime import date
+import keyboard
 
 class Listing(models.Model):
     pokemon = models.ForeignKey("collection.Pokemon", on_delete=models.CASCADE, default=1) 
@@ -7,4 +9,24 @@ class Listing(models.Model):
     date_posted = models.DateField(auto_now=True)
     status = models.CharField(max_length = 100)
     seller = models.ForeignKey("accounts.profile", on_delete=models.CASCADE, related_name="seller")
+<<<<<<< Updated upstream
     buyer = models.ForeignKey("accounts.profile", on_delete=models.CASCADE, related_name="buyer")
+=======
+    buyer = models.ForeignKey("accounts.profile", on_delete=models.CASCADE, related_name="buyer")
+
+    def post_Pokemon(Pokemon, Collection):
+        if(Profile.contains(Pokemon)):
+            Collection.remove(Pokemon)
+            Listing.Pokemon.price == int(input('Price: '))
+            Listing.date_posted = date.today()
+            Listing.status = 'Not Sold'
+            Listing.seller = User
+            Listing.buyer = null
+
+            Listing.append(Pokemon)
+
+
+
+
+    
+>>>>>>> Stashed changes
